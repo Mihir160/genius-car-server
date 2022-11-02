418343462b4ddf5b16f763532e30e0e8abaede06
Mihir160 <mihir.roy266@gmail.com>
2022-11-02 22:10:29 +0600

    delete update and added

--- index.js
+++ index.js
@@ -56,6 +56,27 @@ async function run(){
                 const result = await oderCollection.insertOne(order)
                 res.send(result)
                })
+
+               app.delete('/orders/:id', async(req, res) =>{
+                const id = req.params.id
+                const query = {_id: ObjectId(id)}
+                const result = await oderCollection.deleteOne(query)
+                res.send(result)
+               })
+
+               app.patch('/orders/:id', async(req, res) =>{
+                const id = req.params.id
+                const status = req.body.status
+                const query = {_id: ObjectId(id)}
+                const updatedDoc = {
+                    $set:{
+                        status: status
+                    }
+                }
+                const result = await oderCollection.updateOne(query, updatedDoc)
+                res.send(result)
+
+             })
           }
           finally{
 
